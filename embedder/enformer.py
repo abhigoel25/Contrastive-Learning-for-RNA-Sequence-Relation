6af9c6aebbcffd5ed60f56d50e3c647c43ba9ee4
Abhinav Goel <abhinavgoel115@gmail.com>
2025-10-30 23:34:42 -0400

    Delete embedder directory

--- embedder/enformer.py
+++
@@ -1,9 +0,0 @@
-from enformer_pytorch import from_pretrained
-from src.embedder.base import BaseEncoder
-
-
-class EnformerEmbedder(BaseEncoder):
-    
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.backbone = from_pretrained(self.model_name, use_tf_gamma = False)
